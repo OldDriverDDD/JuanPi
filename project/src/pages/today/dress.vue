3908d2d66060645f8a46f8f05e3d6f6cc4e41e33
you <you@example.com>
2017-06-05 21:40:36 +0800

    change

---
+++ project/src/pages/today/dress.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="dress">
+		<h4>nihaoi </h4>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		name:"dress",
+		data(){
+			return {
+				data:[]
+			}
+		},
+		created(){
+			this.axios.get('../../../static/data/wClass.json').then(res=>{
+				console.log(res);
+			})
+		}
+	}
+</script>
+<style type="text/css" scoped>
+	
+</style>
