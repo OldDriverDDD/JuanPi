03f02c759dae1267c96e29be1f4e7c3cfc7408c3
you <you@example.com>
2017-06-06 11:43:48 +0800

    direct.vue

--- project/src/pages/direct/direct.vue
+++ project/src/pages/direct/direct.vue
@@ -1,18 +1,46 @@
 <template>
 	<div id="direct">
-		<h2>直发仓</h2>
+		<div class="header">
+			<span>生活仓库</span>
+			<i></i>
+		</div>
+		<ul class="bannerF" v-for="item in bannerDataO">
+			<li><img :src="item.pic" alt=""></li>
+		</ul>
+		<ul v-for="item in bannerDataT">
+			<li><img :src="item.pic" alt=""></li>
+		</ul>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: "direct"
+		name: "direct",
+		data(){
+			return {
+				bannerDataO: [],
+				bannerDataT: []
+			}
+		},
+		created(){
+			this.axios.get('../static/data/directBanner.json').then(res => {
+				// this.data = res.module_ads.multi_block.data.child;
+				this.bannerDataO = res.data.module_ads.multi_block[0].data[0].child;
+				this.bannerDataT = res.data.module_ads.multi_block[1].data[0].child;
+
+				console.log(res.data.module_ads.multi_block[1].data[0].child);
+			},err => {
+				console.log(err);
+			})
+		}
 	}
 </script>
 
 <style>
-	h2 {
-		color: deeppink;
-		font-size: 50px;
+	#direct{
+		font-family: "微软雅黑";
+	}
+	#direct .header span{
+		font-size: 54px;
 	}
 </style>
